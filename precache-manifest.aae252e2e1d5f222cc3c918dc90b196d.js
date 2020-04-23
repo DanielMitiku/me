82480daf17c47da3e0e5597e7496c6098675074c
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-23 15:22:40 +0300

    Updates

---
+++ precache-manifest.aae252e2e1d5f222cc3c918dc90b196d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90550d14e3fea1fad259abc3ceb3d02d",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "9f6b2871280e52511ce4",
+    "url": "/me/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "4b748c233894b7b2f9de",
+    "url": "/me/static/css/main.d3611f42.chunk.css"
+  },
+  {
+    "revision": "9f6b2871280e52511ce4",
+    "url": "/me/static/js/2.1691770d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.1691770d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b748c233894b7b2f9de",
+    "url": "/me/static/js/main.72eb0837.chunk.js"
+  },
+  {
+    "revision": "b345aedcd2dbd7427fcf",
+    "url": "/me/static/js/runtime-main.880a4756.js"
+  },
+  {
+    "revision": "f0c4676d42a7f655d2e1bbc77ce8f6b4",
+    "url": "/me/static/media/battleship.f0c4676d.png"
+  },
+  {
+    "revision": "f322c56e41104acbc4a5b0d0f646ce48",
+    "url": "/me/static/media/book_a_session.f322c56e.png"
+  },
+  {
+    "revision": "eab81817a8c3c6f012d54dc0b0405b54",
+    "url": "/me/static/media/book_a_session_backend.eab81817.png"
+  },
+  {
+    "revision": "518abbb3f4f26bdf8dbb17f56ac00ceb",
+    "url": "/me/static/media/rails_book.518abbb3.png"
+  },
+  {
+    "revision": "00f06e08a639d086296c3a116017c122",
+    "url": "/me/static/media/weather_app.00f06e08.png"
+  }
+]);
